bf4d57b2303ac158c5144881cb62b107df462895
Daniel Ilchyshyn <kruberlick@gmail.com>
2016-04-16 21:57:08 +0300

    Added Node class which represents a tree's node

---
+++ src/binary_search_tree/Node.java
@@ -0,0 +1,25 @@
+package binary_search_tree;
+
+public class Node<T> {
+    private T value;
+    private Node<T> leftChild;
+    private Node<T> rightChild;
+    public void setValue(T value) {
+        this.value = value;
+    }
+    public void setLeftChild(Node<T> child) {
+        leftChild = child;
+    }
+    public void setRightChild(Node<T> child) {
+        rightChild = child;
+    }
+    public T getValue() {
+        return value;
+    }
+    public Node<T> getLeftChild() {
+        return leftChild;
+    }
+    public Node<T> getRightChild() {
+        return rightChild;
+    }
+}
